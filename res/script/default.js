4a7a784aac02204cb9c2575064468f06a3d954c4
Brooklyn Huang <brooklyn.huang@sap.com>
2012-10-31 12:44:38 +0800

    add images

--- res/script/default.js
+++ res/script/default.js
@@ -1,11 +1,18 @@
 (function(){
 	var kila = {};
 
-	kila.images = function(){
-		this.width = "30px";
-		this.height = "60px";
-		this.src = "";
-	}
+	kila.image = function(small,large){
+		this.img_path = "res/images/";
+		this.small = this.img_path + small;
+		this.large = this.img_path + large;
+	};
+
+	kila.image.prototype.content = function(){
+		var $content = $("<a  href='" + this.large + "' class='fancy'/>"),
+			$img = $("<img src='" + this.small + "' />");
+		$content.append($img);
+		return $content;
+	};
 
 	window.kila = kila;
 })();
